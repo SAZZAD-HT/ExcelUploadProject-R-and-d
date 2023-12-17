b5cb44c410c8f2af27d37577ebfd3610cbffb844
SAZZAD-HT <sazzadaiub1@gmail.com>
2023-12-17 16:57:42 +0600

    Sheet Name Wise Excel Read

--- ExcelUpload.js
+++ ExcelUpload.js
@@ -4,12 +4,27 @@ var ExcelPopulate = {
         try {
             var input = document.getElementById('fileInput');
             var file = input.files[0];
-console.log(file);  
+
             if (file) {
                 var arrayBuffer = await file.arrayBuffer();
                 var data = new Uint8Array(arrayBuffer);
                 var workbook = XLSX.read(data, { type: 'array' });
-                console.log(workbook);
+
+                // Specify the sheet name you want to process
+                var targetSheetName = 'Bln_fil';
+
+                if (workbook.Sheets[targetSheetName]) {
+                    var roa = XLSX.utils.sheet_to_json(workbook.Sheets[targetSheetName], { header: 1 });
+                    if (roa.length) result.push(roa);
+
+                    let html = XLSX.utils.sheet_to_html(workbook.Sheets[targetSheetName]);
+                    // Assuming you have a div with id "table" in your HTML
+                    document.getElementById('table').innerHTML += `
+                        <h3>${targetSheetName}</h3>${html}
+                    `;
+                } else {
+                    console.error('Sheet not found:', targetSheetName);
+                }
             } else {
                 console.error('No file selected.');
             }
@@ -19,5 +34,4 @@ console.log(file);
 
         return result;
     }
-    
 };
