aea8383c75ef91185009f4e62a60899a65c3345e
SAZZAD-HT <sazzadaiub1@gmail.com>
2023-12-18 10:00:18 +0600

    ss

--- ExcelUpload.js
+++ ExcelUpload.js
@@ -1,6 +1,5 @@
 var ExcelPopulate = {
     getDataofExcel: async function () {
-        var result = [];
         try {
             var input = document.getElementById('fileInput');
             var file = input.files[0];
@@ -10,20 +9,55 @@ var ExcelPopulate = {
                 var data = new Uint8Array(arrayBuffer);
                 var workbook = XLSX.read(data, { type: 'array' });
 
-                // Specify the sheet name you want to process
-                var targetSheetName = 'Bln_fil';
+                // Assume you want to fetch data from the first sheet (index 0)
+                var targetSheetIndex = 1;
+                var targetSheet = workbook.Sheets[workbook.SheetNames[targetSheetIndex]];
 
-                if (workbook.Sheets[targetSheetName]) {
-                    var roa = XLSX.utils.sheet_to_json(workbook.Sheets[targetSheetName], { header: 1 });
-                    if (roa.length) result.push(roa);
+                if (targetSheet) {
+                    var rows = XLSX.utils.sheet_to_json(targetSheet, { header: 1 });
 
-                    let html = XLSX.utils.sheet_to_html(workbook.Sheets[targetSheetName]);
-                    // Assuming you have a div with id "table" in your HTML
+                    // You can access the data by index (rows) and column (header)
+                    for (var i = 0; i < rows.length; i++) {
+                        for (var j = 0; j < rows[i].length; j++) {
+                           //console.log(`Row: ${i}, Column: ${j}, Value: ${rows[i][j]}`);
+                        }
+                    }
+
+                    var targetCellIndex = XLSX.utils.encode_cell({ r: 3, c: 0 });
+                    var cellValueindex = targetSheet[targetCellIndex]?.v;
+                    //console.log(cellValueindex);
+                    debugger;
+                    var targetCell = 'A2';
+                    var cellValue = targetSheet[targetCell]?.v;
+                   // console.log(cellValue);
+                    var inputBox = document.getElementById('inputBox');
+                    if (inputBox) {
+                        inputBox.value = cellValue;
+                    }    
+                        //InputDate 
+                        var targetCell = 'N4';     
+                        cellValue = new Date().toLocaleString();   
+                        var inputBox = document.getElementById('inputDate');
+                        if (inputBox) {
+                            inputBox.value = cellValue;
+                        }           
+                            
+                        //StyleName
+                        targetCell = 'C6';
+                        document.getElementById('styleName').value = targetSheet[targetCell]?.v;
+
+                        //Buyer Name
+                        targetCell = 'C4';
+                        document.getElementById('buyerName').value = targetSheet[targetCell]?.v;
+
+
+                    console.log(rows);
+                    let html = XLSX.utils.sheet_to_html(targetSheet);
                     document.getElementById('table').innerHTML += `
-                        <h3>${targetSheetName}</h3>${html}
+                        <h3>Data from Sheet ${targetSheetIndex}</h3>${html}
                     `;
                 } else {
-                    console.error('Sheet not found:', targetSheetName);
+                    console.error('Sheet not found:', targetSheetIndex);
                 }
             } else {
                 console.error('No file selected.');
@@ -31,7 +65,5 @@ var ExcelPopulate = {
         } catch (error) {
             console.error('Error:', error);
         }
-
-        return result;
     }
 };
