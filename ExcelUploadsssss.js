747915e47c8ea9e70e33a7160d48c59ba5923d2e
SAZZAD-HT <sazzadaiub1@gmail.com>
2023-12-19 09:02:01 +0600

    Table Data Add Remove And Delete

---
+++ ExcelUploadsssss.js
@@ -0,0 +1,35 @@
+// ... (your existing code)
+
+// Add the "Action" column header
+headerRow.insertCell(list[0].length).textContent = "Action";
+
+for (var i = 0; i < list.length; i++) {
+    var row = table.insertRow(-1);
+
+    for (var j = 0; j < list[i].length; j++) {
+        var l = j;
+        var cell = row.insertCell(j >= 9 ? j - 2 : j);
+
+        if (j !== 7 && j !== 8) {
+            if (j > 3 && j != 6 && j != 12) {
+                cell.textContent = parseFloat(list[i][l]).toFixed(2);
+            } else {
+                if (j == 6 || j == 12) {
+                    cell.textContent = Math.abs(list[i][j]) % 1 === 0.5 ? Math.floor(list[i][j]) : Math.round(list[i][j]);
+                } else {
+                    cell.textContent = list[i][l];
+                }
+            }
+        }
+
+        // Add a button in the "Action" column for the last column
+        if (j === list[i].length - 1) {
+            var button = document.createElement('button');
+            button.textContent = 'Edit'; // Change this text as needed
+            button.classList.add('action-button');
+            cell.appendChild(button);
+        }
+    }
+}
+
+// ... (your existing code)
---
+++ tabledatAdd.js
@@ -0,0 +1,90 @@
+let currentId = 1; // Variable to keep track of the current ID
+
+function addOrUpdateRow() {
+    const nameInput = document.getElementById('nameInput').value;
+
+    if (nameInput.trim() === '') {
+        alert('Please enter a name.');
+        return;
+    }
+
+    const table = document.getElementById('dataTable');
+    const tbody = table.getElementsByTagName('tbody')[0];
+
+    // Check if the name already exists in the table
+    const existingRow = findRowByName(nameInput);
+
+    if (existingRow) {
+        // If the name exists, update the existing row
+        const id = existingRow.cells[0].textContent;
+        updateRow(existingRow, id, nameInput);
+    } else {
+        // If the name doesn't exist, add a new row
+        addRow(tbody, currentId, nameInput);
+        currentId++; // Increment the ID for the next entry
+    }
+
+    // Clear the input field
+    document.getElementById('nameInput').value = '';
+}
+
+function addRow(tbody, id, name) {
+    const newRow = tbody.insertRow();
+    const cellId = newRow.insertCell(0);
+    const cellName = newRow.insertCell(1);
+    const cellAction = newRow.insertCell(2);
+
+    cellId.textContent = id;
+    cellName.textContent = name;
+
+    // Add Edit and Delete buttons
+    const editButton = document.createElement('button');
+    editButton.textContent = 'Edit';
+    editButton.onclick = function () {
+        editRow(newRow);
+    };
+
+    const deleteButton = document.createElement('button');
+    deleteButton.textContent = 'Delete';
+    deleteButton.onclick = function () {
+        deleteRow(newRow);
+    };
+
+    cellAction.appendChild(editButton);
+    cellAction.appendChild(deleteButton);
+}
+
+function updateRow(row, id, name) {
+    row.cells[0].textContent = id;
+    row.cells[1].textContent = name;
+}
+
+function editRow(row) {
+    const id = row.cells[0].textContent;
+    const name = row.cells[1].textContent;
+
+    // Populate the input field with the existing data
+    document.getElementById('nameInput').value = name;
+
+    // Remove the existing row
+    row.parentNode.removeChild(row);
+}
+
+function deleteRow(row) {
+    row.parentNode.removeChild(row);
+}
+
+function findRowByName(name) {
+    const table = document.getElementById('dataTable');
+    const rows = table.getElementsByTagName('tbody')[0].getElementsByTagName('tr');
+
+    for (let i = 0; i < rows.length; i++) {
+        const rowName = rows[i].cells[1].textContent;
+
+        if (rowName === name) {
+            return rows[i];
+        }
+    }
+
+    return null;
+}
